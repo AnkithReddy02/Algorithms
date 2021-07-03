d95c6bdfe06e11241ec2e66460d8ab3b572c20e8
Ankith Reddy <ankithreddy2001@gmail.com>
2021-07-03 13:22:40 +0530

    Create DSU.cpp

---
+++ AdvancedAlgo/DSU.cpp
@@ -0,0 +1,28 @@
+#define N 1005
+    int sizex[N];
+    int parent[N];
+    
+    int findset(int x)
+    {
+        if(parent[x]==x)
+        {
+            return x;
+        }
+        
+        return parent[x] = findset(parent[x]);
+    }
+    void unionset(int a,int b)
+    {
+        int x = findset(a);
+        int y = findset(b);
+        
+        if(x!=y)
+        {
+            if(sizex[y] > sizex[x])
+            {
+                swap(x,y);
+            }
+            sizex[x] += sizex[y];
+            parent[y] = x;
+        }
+    }
